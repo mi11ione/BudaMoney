df9082b7981abb8a202983175d04172076f5e3eb
F0x1d <i@f0x1d.com>
2024-05-31 21:35:42 +0300

    [feat]: added settings

--- BudaMoney/BudaMoney/Sources/View/Settings/SettingsView.swift
+++ BudaMoney/BudaMoney/Sources/View/Settings/SettingsView.swift
@@ -12,7 +12,23 @@ struct SettingsView: View {
     
     var body: some View {
         List {
+            Section("Application") {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("BudaMoney")
+                        .bold()
+                    Text("Clicker history game")
+                }
+            }
             
+            Section("Authors") {
+                Link("f0x1d", destination: URL(string: "https://t.me/f0x1d")!)
+                Link("mi11ion", destination: URL(string: "https://t.me/mi11ion")!)
+            }
         }
+        .navigationTitle("Settings")
     }
+}
+
+#Preview {
+    SettingsView()
 }
